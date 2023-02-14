88f31f652476032cc7e638523897152b14206a98
del-khay <driss.elkhayyate@outlook.com>
2023-02-14 17:36:02 +0100

    update

--- execute/sigs.c
+++ execute/sigs.c
@@ -6,7 +6,7 @@
 /*   By: del-khay <del-khay@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 19:03:32 by del-khay          #+#    #+#             */
-/*   Updated: 2023/02/14 17:15:53 by del-khay         ###   ########.fr       */
+/*   Updated: 2023/02/14 17:22:04 by del-khay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ void	sigint_handler(int sig)
 {
 	int	i;
 
-	i = sig;
 	i = -1;
 	if (!g_gfl.pid && !g_gfl.crp)
 	{
@@ -25,7 +24,12 @@ void	sigint_handler(int sig)
 	}
 	else if(g_gfl.pid && g_gfl.crp) 
 	{
+		if (g_gfl.crp == 1)
+		{
+			kill(g_gfl.pid[0], SIGKILL);
+			return ;
+		}
 		while (++i < g_gfl.crp)
-			kill(g_gfl.pid[i], SIGKILL);
+			kill(g_gfl.pid[i], sig);
 	}
 }

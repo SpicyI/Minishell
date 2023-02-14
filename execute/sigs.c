47025ebe7b2ecf7744188e4be22d03d9f65b40b7
del-khay <driss.elkhayyate@outlook.com>
2023-02-14 17:16:43 +0100

    killing herdoc cause main to sigint

--- execute/sigs.c
+++ execute/sigs.c
@@ -6,7 +6,7 @@
 /*   By: del-khay <del-khay@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 19:03:32 by del-khay          #+#    #+#             */
-/*   Updated: 2023/02/13 18:41:08 by del-khay         ###   ########.fr       */
+/*   Updated: 2023/02/14 17:15:53 by del-khay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,16 @@ void	sigint_handler(int sig)
 {
 	int	i;
 
+	i = sig;
 	i = -1;
-	if (g_gfl.pid == 0)
+	if (!g_gfl.pid && !g_gfl.crp)
 	{
 		printf("\n");
 		return ;
 	}
-	else
+	else if(g_gfl.pid && g_gfl.crp) 
 	{
 		while (++i < g_gfl.crp)
-			kill(g_gfl.pid[i], sig);
+			kill(g_gfl.pid[i], SIGKILL);
 	}
 }
